7978dfa9bdd2134f5f46ea7460da1b67229beca1
Brian West <WestBrian@users.noreply.github.com>
2022-08-31 21:20:33 -0400

    feat: add my account page (#16)

---
+++ firebase.jest.config.js
@@ -0,0 +1,4 @@
+module.exports = {
+  testEnvironment: 'node',
+  transform: { '\\.[jt]sx?$': 'ts-jest' },
+}
--- src/components/FirebaseProviders.tsx
+++ src/components/FirebaseProviders.tsx
@@ -1,13 +1,58 @@
-import React, { ReactNode, type FC } from 'react'
-import { AuthProvider, useFirebaseApp } from 'reactfire'
-import { getAuth } from 'firebase/auth'
+/* istanbul ignore file */
+import React, { useState, useEffect, type ReactNode, type FC } from 'react'
+import { AuthProvider, FirestoreProvider, useFirebaseApp } from 'reactfire'
+import { getAuth, connectAuthEmulator } from 'firebase/auth'
+import {
+  getFirestore,
+  connectFirestoreEmulator,
+  doc,
+  getDoc,
+  setDoc,
+} from 'firebase/firestore'
+import { isDev, isBrowser } from '../utils/isEnv'
+
+const doNotMockDb = process.env.NEXT_PUBLIC_DB_PROD === 'enabled'
 
 export interface FirebaseProvidersProps {
   children: ReactNode
 }
 
 export const FirebaseProviders: FC<FirebaseProvidersProps> = ({ children }) => {
-  const authInstance = getAuth(useFirebaseApp())
+  const app = useFirebaseApp()
+  const auth = getAuth(app)
+  const firestore = getFirestore(app)
+  const [uid, setUid] = useState<string | null>(null)
 
-  return <AuthProvider sdk={authInstance}>{children}</AuthProvider>
+  if (isDev && isBrowser && !doNotMockDb && !auth.emulatorConfig) {
+    console.log('--- 🔧 Setting up emulators 🔧 ---')
+    connectAuthEmulator(auth, 'http://localhost:9099')
+    connectFirestoreEmulator(firestore, 'localhost', 8080)
+  }
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((user) => {
+      if (user) {
+        setUid(user.uid)
+      }
+    })
+
+    return () => unsubscribe()
+  }, [auth, setUid])
+
+  useEffect(() => {
+    if (uid) {
+      const userRef = doc(firestore, 'users', uid)
+      getDoc(userRef).then((snapshot) => {
+        if (!snapshot.exists()) {
+          setDoc(userRef, {})
+        }
+      })
+    }
+  }, [uid, firestore])
+
+  return (
+    <AuthProvider sdk={auth}>
+      <FirestoreProvider sdk={firestore}>{children}</FirestoreProvider>
+    </AuthProvider>
+  )
 }
